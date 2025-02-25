400826aed6d2cea3dcdbe6751ca17f4370bb5ca1
VanshKhandelia <khandeliavansh@gmail.com>
2025-02-25 15:37:40 -0800

    some comments

--- functions.php
+++ functions.php
@@ -1,5 +1,8 @@
 <?php
 function school_register_custom_post_types() {
+    //Registring Students post type
+
+    //registering staff post type
 }
 add_action( 'init', 'school_register_custom_post_types' );
 ?>
