939164ed361237a7b78b0d2713663b530a030c54
VanshKhandelia <khandeliavansh@gmail.com>
2025-02-25 15:40:59 -0800

    made some adjustments for file organisation

--- functions.php
+++ functions.php
@@ -1,8 +1,3 @@
 <?php
-function school_register_custom_post_types() {
-    //Registring Students post type
-
-    //registering staff post type
-}
-add_action( 'init', 'school_register_custom_post_types' );
+require get_template_directory() . '/inc/post-types-taxonomies.php';
 ?>
