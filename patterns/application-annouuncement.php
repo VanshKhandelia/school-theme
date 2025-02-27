5d2edf66ed672a7b61ec93a5d7d5daee5625ce0f
VanshKhandelia <khandeliavansh@gmail.com>
2025-02-26 17:09:14 -0800

    added a theme pattern

---
+++ patterns/application-annouuncement.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Application Announcement
+ * Slug: school-theme/application-announcement
+ * Category: text
+ */
+?>
+
+
+<!-- wp:paragraph {"align":"center","fontSize":"large"} -->
+<p class="has-text-align-center has-large-font-size">Now Accepting Applications for:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|accent"}}}},"textColor":"accent","fontSize":"x-large"} -->
+<p class="has-text-align-center has-accent-color has-text-color has-link-color has-x-large-font-size">Fall 2025</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"center","fontSize":"medium"} -->
+<p class="has-text-align-center has-medium-font-size">Don't miss out on your future!</p>
+<!-- /wp:paragraph -->
